054c1f17ed9709769cf813951b9e8e4a1ffca801
Simon Kirkby <obeygiantrobot@gmail.com>
2022-12-10 07:10:26 +0800

    separate the web interface and move to css

--- files/web_interface.py
+++ files/web_interface.py
@@ -4,8 +4,8 @@ import picoweb
 app = picoweb.WebApp(__name__)
 
 
-def stream_file(file_name, resp):
-    yield from picoweb.start_response(resp)
+def stream_file(resp,file_name, content='text/html'):
+    yield from picoweb.start_response(resp,content_type=content)
     htmlFile = open(file_name, "r")
     # for line in htmlFile:
     #    yield from resp.awrite(line)
@@ -20,11 +20,15 @@ def stream_file(file_name, resp):
 # the default landing page
 @app.route("/")
 def index(req, resp):
-    yield from stream_file('static/index.html',resp)
+    yield from stream_file(resp,'static/index.html')
 
 @app.route("/controller")
 def controller(req,resp):
-    yield from stream_file('static/controller.html',resp)
+    yield from stream_file(resp,'static/controller.html')
+
+@app.route("/app.css")
+def css(req,resp):
+    yield from stream_file(resp,'static/app.css',content="text/css")
 
 @app.route("/status")
 def status(req, resp):
